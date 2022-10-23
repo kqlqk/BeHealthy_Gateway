67820bc3908bf1e0cb223f90afaf5129ee69553b
kqlqk <kononchuk99999@gmail.com>
2022-10-23 15:26:44 +0200

    prepare for deploy on tomcat

--- src/main/java/me/kqlqk/behealthy/gateway/Gateway.java
+++ src/main/java/me/kqlqk/behealthy/gateway/Gateway.java
@@ -2,21 +2,14 @@ package me.kqlqk.behealthy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableFeignClients
 public class Gateway extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Gateway.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Gateway.class);
     }
 }
