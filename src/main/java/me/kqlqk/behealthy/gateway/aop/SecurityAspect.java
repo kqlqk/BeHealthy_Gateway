2211773f24702ffb2df823eb8e865dab7f25d2c4
kqlqk <kononchuk99999@gmail.com>
2023-01-19 13:49:34 +0100

    update SecurityAspect.java
    * was updated SecurityAspect.java

--- src/main/java/me/kqlqk/behealthy/gateway/aop/SecurityAspect.java
+++ src/main/java/me/kqlqk/behealthy/gateway/aop/SecurityAspect.java
@@ -1,18 +1,17 @@
 package me.kqlqk.behealthy.gateway.aop;
 
 import lombok.extern.slf4j.Slf4j;
-import me.kqlqk.behealthy.gateway.dto.authenticationService.TokensDTO;
 import me.kqlqk.behealthy.gateway.dto.authenticationService.UserDTO;
-import me.kqlqk.behealthy.gateway.exception.exceptions.authenticationService.TokenNotFoundException;
-import me.kqlqk.behealthy.gateway.exception.exceptions.authenticationService.UserException;
 import me.kqlqk.behealthy.gateway.feign_client.AuthenticationClient;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -28,23 +27,25 @@ public class SecurityAspect {
     }
 
     @Before("@annotation(CheckUserId)")
-    private void beforeCheckUserIdAnnotation(JoinPoint joinPoint) {
+    private void beforeCheckUserIdAnnotation(JoinPoint joinPoint) throws NoSuchFieldException {
         long requestUserId = 0;
+        Method m = ((MethodSignature) joinPoint.getSignature()).getMethod();
 
-        for (Object arg : joinPoint.getArgs()) {
-            if (arg instanceof Long) {
-                requestUserId = (long) arg;
+        for (int i = 0; i < m.getParameters().length; i++) {
+            if (m.getParameters()[i].getName().equals("id")) {
+                requestUserId = (long) joinPoint.getArgs()[i];
+                break;
             }
         }
 
         String accessToken = getTokenFromRequest(request);
-        String email = authenticationClient.getEmailFromAccessToken(new TokensDTO(accessToken, null)).get("email");
+        String email = authenticationClient.getEmailFromAccessToken(accessToken).get("email");
         UserDTO userDTO = authenticationClient.getUserByEmail(email);
 
         if (userDTO.getId() != requestUserId) {
             log.info(userDTO.getEmail() + " failed access to " + request.getRequestURI() + " (securityAspect)");
 
-            throw new UserException("Wrong id! Your id = " + userDTO.getId());
+            throw new RuntimeException("Wrong id! Your id = " + userDTO.getId());
         } else {
             log.info(userDTO.getEmail() + " got access to " + request.getRequestURI() + " (securityAspect)");
         }
@@ -57,8 +58,8 @@ public class SecurityAspect {
             if (authHeader.startsWith("Bearer ")) {
                 return authHeader.substring(7);
             }
-            throw new TokenNotFoundException("Bearer token not found");
+            throw new RuntimeException("Bearer token not found");
         }
-        throw new TokenNotFoundException("Authorization header not found");
+        throw new RuntimeException("Authorization header not found");
     }
 }
